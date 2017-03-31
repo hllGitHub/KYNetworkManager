96e5ccb2dbf4af1101fa4634cc60af37b2fc04b1
Jeffrey <jeffrey.hu@thecarevoice.com>
2017-03-31 11:39:49 +0800

    Initial Commit

---
+++ KYNetworkManager/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  KYNetworkManager
+//
+//  Created by Jeffrey hu on 17/3/31.
+//  Copyright © 2017年 Jeffrey hu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ KYNetworkManager/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  KYNetworkManager
+//
+//  Created by Jeffrey hu on 17/3/31.
+//  Copyright © 2017年 Jeffrey hu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
